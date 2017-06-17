a763233725ad936b52f05f69d5dd1cc04371c7b8
Ivan Jager <aij+git@mrph.org>
2017-06-17 12:26:13 -0500

    Initial standard Nix config

---
+++ standard.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "dvorak";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  environment.systemPackages = with pkgs; [
+     wget vim screen latencytop powertop htop lsof psmisc pwgen traceroute mtr tree tcpdump zip unzip pciutils ethtool sdparm lsscsi rlwrap
+     file usbutils bsdgames fping hdparm iotop finger_bsd openssl inetutils unar smartmontools
+     lynx w3m
+     git ack binutils ocaml emacs 
+  ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.ivan = {
+     isNormalUser = true;
+     uid = 1000;
+     extraGroups = [ "cdrom" ];
+   };
+  users.extraUsers.aij = {
+    isNormalUser = true;
+    uid = 1001;
+  };
+} 
