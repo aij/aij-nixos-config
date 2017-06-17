a4db94d35d2f49a15d53a09c887d9bf55a240fcd
Ivan Jager <aij+git@mrph.org>
2017-06-17 14:04:11 -0500

    Create desktop.nix

---
+++ desktop.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ./standard.nix ];
+
+  environment.systemPackages = with pkgs; [
+     gkrellm
+     gnupg kismet # openssh_with_kerberos
+     xscreensaver rxvt_unicode terminus_font pidgin geeqie mplayer vlc mpv
+     xorg.xinit xorg.xdpyinfo xorg.xbacklight xorg.xdriinfo xfontsel x2x
+     chromium  firefox google-chrome # chromiumDev
+     evince gphoto2 gphoto2fs
+     enlightenment.rage enlightenment.terminology
+     trayer haskellPackages.xmobar
+     audacity fldigi
+     exif exiftags
+     keepassx2
+     gnumake scala jdk python python3 ack clang ocamlPackages.utop # jre
+     androidsdk
+     lsdvd dvdbackup handbrake      
+  ];
+
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    enableDefaultFonts = true;
+    fonts = with pkgs; [
+        terminus_font
+    ];
+  };
+
+} 
