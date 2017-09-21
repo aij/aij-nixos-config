c6f865d2daf31ce0880f6781ad0426e75e0f8769
Ivan Jager <aij+git@mrph.org>
2017-09-21 15:18:23 -0500

    More ccap-dev

--- ccap-dev.nix
+++ ccap-dev.nix
@@ -2,9 +2,12 @@
 {
   imports = [ ./dev.nix ];
 
+  networking.firewall.allowedTCPPorts = [ 8080 ];
+
   environment.systemPackages = with pkgs; [
      # Packages needed for CCAP3 development
-     nodejs yarn postgresql
+     nodejs yarn purescript
+     postgresql
      (python.withPackages(ps: with ps; [ lxml beautifulsoup4 ]))
      google-chrome-beta remmina 
   ];
