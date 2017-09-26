69cd5b03c50b838d935e40fa63b2a6a34165bf56
Ivan Jager <aij+git@mrph.org>
2017-09-26 15:25:10 -0500

    Add nix-repl, dash, and zsh

--- dev.nix
+++ dev.nix
@@ -5,9 +5,11 @@
   environment.systemPackages = with pkgs; [
      man-pages wdiff
      gnumake gcc scala jdk ack ag clang ocamlPackages.utop # jre
+     dash zsh
      python python3
      #androidsdk
      rustc cargo
      sshfs-fuse
+     nix-repl
   ];
 } 
