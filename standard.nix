510776b5d6ce17f00d28394a560f2db2fbeb0b8f
Ivan Jager <aij+git@mrph.org>
2017-10-19 10:21:12 -0500

    re-enable command-not-found

--- standard.nix
+++ standard.nix
@@ -22,6 +22,9 @@
      (import pkg/emacs.nix { inherit pkgs; })
   ];
 
+  # Default changed in 5a5db609e5bd83bc589f36eef394f3ad172d6648 and 9df79de1a115920bd96a7d4e66bc3782865c8146
+  programs.command-not-found.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.ivan = {
      isNormalUser = true;
@@ -32,4 +35,4 @@
     isNormalUser = true;
     uid = 1001;
   };
-} 
+}
