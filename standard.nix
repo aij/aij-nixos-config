a9a40bcee92c63924e48d533f1df354962173d64
Ivan Jager <aij+git@mrph.org>
2017-10-29 17:31:05 -0500

    +numactl (for numastat)

--- standard.nix
+++ standard.nix
@@ -13,7 +13,7 @@
 
   environment.systemPackages = with pkgs; [
      wget vim screen latencytop powertop htop lsof psmisc pwgen traceroute mtr tree tcpdump zip unzip pciutils ethtool sdparm lsscsi rlwrap
-     file usbutils bsdgames fping hdparm iotop finger_bsd openssl inetutils unar smartmontools sysstat beep
+     file usbutils bsdgames fping hdparm iotop finger_bsd openssl inetutils unar smartmontools sysstat beep numactl
      zfs btrfs-progs xfsprogs
      lynx w3m
      git ack binutils ocaml
