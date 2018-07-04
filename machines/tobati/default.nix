f2f39f21bf793d0a0392e6d47c881f4a951f977b
Ivan Jager <aij+git@mrph.org>
2018-07-04 14:20:32 -0500

    tobati: +amdgpu

--- machines/tobati/default.nix
+++ machines/tobati/default.nix
@@ -6,13 +6,16 @@
     ../../sshd.nix
     ../../desktop.nix
     ../../dev.nix
-    ../../profiles/miner.nix
+    # ../../profiles/miner.nix
   ];
 
   networking.hostName = "tobati";
   networking.hostId = "235d6160";
 
   services.openssh.enable = true;
+
+  services.xserver.videoDrivers = [ "amdgpu" "modesetting" ];
+  boot.kernelPackages = pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor pkgs.linux_4_17);
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
