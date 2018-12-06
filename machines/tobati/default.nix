9f5124fd567c934281c932fd8a7d284fe72614f7
Ivan Jager <aij+git@mrph.org>
2018-12-05 19:52:01 -0600

    tobati: linux: 4.18 -> latest (4.19.6)
    4.18 is no longer in nixos-unstable

--- machines/tobati/default.nix
+++ machines/tobati/default.nix
@@ -16,7 +16,7 @@
   services.openssh.enable = true;
 
   services.xserver.videoDrivers = [ "amdgpu" "modesetting" ];
-  boot.kernelPackages = pkgs.linuxPackages_4_18;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
