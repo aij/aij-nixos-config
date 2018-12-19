826223d53ccbdcfc7d63298d065b5c141e0f740f
Ivan Jager <aij+git@mrph.org>
2018-12-18 20:10:59 -0600

    Extract machine configs from nixops repo

---
+++ machines/m1/default.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, nodes, ... }: {
+  # Cisco C220, 2x E5-2620
+  networking.hostName = "m1"; # Formerly lff
+  networking.hostId = "089eebf7";
+  system.stateVersion = "17.03";
+
+  imports =
+    [ ./hardware-configuration.nix
+      ../../zfs.nix
+      ../../standard.nix
+    ];
+}
