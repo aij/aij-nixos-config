5005c1f9e68db50399afc022c4cadd70e93db097
Ivan Jager <aij+git@mrph.org>
2019-01-16 23:59:59 -0600

    Revert "tobati: hack: Overlay efibootmgr to bisect broken nixpkgs"
    This reverts commit 26ce8664c85f01722d7f41ace351a162bb621900.
    
    The bad commit was 374a672424f9407ac5c3f66578e42b7fa8775c34

--- machines/tobati/default.nix
+++ machines/tobati/default.nix
@@ -29,15 +29,4 @@
 
   system.stateVersion = "18.03"; # Did you read the comment?
 
-  # Hack for getting efibootmgr while testing nixpkgs commits where it doesn't compile
-  nixpkgs.overlays = [ (self: super: { 
-    efibootmgr = 
-      let oldpkgs = import (builtins.fetchGit {
-        name = "nixos-unstable-older-efibootmgr-hack";
-        url = https://github.com/nixos/nixpkgs/;
-        rev = "3ab361cb57b1c37a46b568e97d2e5025a1361c0c";
-      }) {};
-      in oldpkgs.efibootmgr;
-   }) ];
-
 }
