29457e1c2acbff1c4c89c068661d4ffa2b2dd2be
Ivan Jager <aij+git@mrph.org>
2019-01-17 21:43:23 -0600

    tobati: Overlay previous, working linux-firmware version

--- machines/tobati/default.nix
+++ machines/tobati/default.nix
@@ -29,4 +29,21 @@
 
   system.stateVersion = "18.03"; # Did you read the comment?
 
+  nixpkgs.overlays = [
+    (self: super: {
+      # The "firmware-linux-nonfree: 20181017 -> 20181213" upgrade causes crashing on boot.
+      # (nixos/nixpkgs@374a672424f9407ac5c3f66578e42b7fa8775c34)
+      # I suspect https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/commit/?id=ec4b0cd394472ee1491df6ef5f215d1f0953f836
+      firmwareLinuxNonfree = super.firmwareLinuxNonfree.overrideAttrs (oldAttrs: {
+        version = "2018-10-17";
+        src = pkgs.fetchgit {
+          url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
+          rev = "de9cefa74bba6fce3834144460868a468b8818f2";
+          sha256 = "101j4jk3ixl8r3mxbkcrr5ybhb44ij3l52js4dqfxpylpiaw2cgk";
+        };
+        outputHash = "1ndwp9yhpmx0kzayddy9i93mpv3d8gxypqm85069ic13lrjz1gdf";
+        });
+    })
+  ];
+
 }
