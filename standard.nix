885f141033a082b507eb75d244605ffd1d02f89c
Ivan Jager <aij+git@mrph.org>
2019-03-19 05:58:09 -0500

    Revert "standard: -bcc for now (broken in unstable)"
    This reverts commit 6e28f8e35761a489747a03ca01e0aa23e9da7b4c.
    
    Building again in unstable as of 1222e289b5014d17884a8b1c99f220c5e3df0b14

--- standard.nix
+++ standard.nix
@@ -24,7 +24,7 @@
      git ack binutils ocaml
      ripgrep # rg
      rxvt_unicode.terminfo
-     # linuxPackages.bcc # broken in unstable 26d8a8c0eb2a88d55694249b099cdd2c89b2b06d
+     linuxPackages.bcc
   ];
 
   # Default changed in 5a5db609e5bd83bc589f36eef394f3ad172d6648 and 9df79de1a115920bd96a7d4e66bc3782865c8146
