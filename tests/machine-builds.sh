7672ecef3400904b75c085be792009ea21a40ada
Ivan Jager <aij+git@mrph.org>
2020-02-01 20:31:27 -0600

    Add auto-update script

--- tests/machine-builds.sh
+++ tests/machine-builds.sh
@@ -12,14 +12,14 @@ for d in machines/*; do
     name=`basename $d`
     for channel in stable unstable; do
 
-	echo "BUILDING: $name $channel"
+	echo "Building: $name $channel"
 	nix-build "<nixpkgs/nixos>" -A system \
 		  -I nixpkgs=$PWD/$channel -I nixos-config=$PWD/machines/$name \
 		  -o result-$name-$channel >logs/$name-$channel.out 2>&1
 	if [ $? == 0 ]; then
-            echo "SUCCESS";
+            echo "Success!";
 	else
-            echo "FAILED";
+            echo "FAILED to build $name $channel";
             rc=1
 	fi
 
---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e -x
+
+# TODO: Check repo cleanliness
+
+(cd unstable && git fetch)
+
+# When using worktrees, this would just re-fetch the same repo, which
+# shouldn't matter since we already have all the updates.
+(cd stable && git fetch)
+
+function do_update () {
+    cd $1
+    old=`git show-ref -s HEAD`
+    git checkout $2
+    # Make sure checkout didn't take us backwards
+    git merge --ff-only $old
+    git merge --ff-only origin/$2
+    cd ..
+}
+
+do_update unstable nixos-unstable
+
+do_update stable nixos-19.09-small
+
+# Is this equivalent, or close enough?
+# git submodule update --remote
+
+# Test that everything still builds
+tests/machine-builds.sh
+
+test -e ../home0.nix && nixops deploy --build-only
+    
+git add stable unstable
+git commit -m 'Update nixpkgs'
