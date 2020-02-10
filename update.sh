6b324b1b41be3b5e8e050f38219ec6bdcc9b9373
Ivan Jager <aij+git@mrph.org>
2020-02-10 07:52:58 -0600

    Try git submodule update instead

--- update.sh
+++ update.sh
@@ -4,14 +4,15 @@ set -e -x
 
 # TODO: Check repo cleanliness
 
-(cd unstable && git fetch)
+#(cd unstable && git fetch)
 
 # When using worktrees, this would just re-fetch the same repo, which
 # shouldn't matter since we already have all the updates.
-(cd stable && git fetch)
+#(cd stable && git fetch)
 
 function do_update () {
     cd $1
+    # FIXME: This gets the wrong commit for the stable worktree.
     old=`git show-ref -s HEAD`
     git checkout $2
     # Make sure checkout didn't take us backwards
@@ -20,12 +21,11 @@ function do_update () {
     cd ..
 }
 
-do_update unstable nixos-unstable
-
-do_update stable nixos-19.09-small
+#do_update unstable nixos-unstable
+#do_update stable nixos-19.09-small
 
 # Is this equivalent, or close enough?
-# git submodule update --remote
+git submodule update --remote
 
 # Test that everything still builds
 tests/machine-builds.sh
