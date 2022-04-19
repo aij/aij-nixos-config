d9d8f782518a3a160f608531b70537cf946df39d
Ivan Jager <aij+git@mrph.org>
2022-04-19 06:27:46 -0500

    bluetooth: Make pulseaudio-modules-bt optional
    For NixOS 21.11. In unstable (and later NixOS 22.05) it has
    been relpaced by pulseaudio 15 native BT support.
    See https://github.com/NixOS/nixpkgs/pull/160097
    and https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/releases#v15.0

--- bluetooth.nix
+++ bluetooth.nix
@@ -1,10 +1,15 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   # See https://nixos.wiki/wiki/Bluetooth
   # and https://wiki.archlinux.org/index.php/Bluetooth_headset
   hardware.bluetooth.enable = true;
   hardware.pulseaudio = {
     enable = true;
-    extraModules = [ pkgs.pulseaudio-modules-bt ];
+    # Used to be necessary for bluetooth headphones
+    # Removed in  https://github.com/NixOS/nixpkgs/pull/160097
+    extraModules =
+      let p = builtins.tryEval pkgs.pulseaudio-modules-bt; # False after NixOS 21.11
+      in
+      lib.optional p.success p.value;
     package = pkgs.pulseaudioFull;
   };
   # services.blueman.enable = true;
