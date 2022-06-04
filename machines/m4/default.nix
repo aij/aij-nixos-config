d898141960dfb12638ea3a2da282ed19cbea205a
Ivan Jager <aij+git@mrph.org>
2022-06-04 10:56:49 -0500

    m4: Fix boot loading
    So I no longer need to select boot device manually. Ended up
    needing to change BIOS settings too since grub couldn't install
    to the disk it was trying to boot from.

--- machines/m4/default.nix
+++ machines/m4/default.nix
@@ -12,7 +12,15 @@
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/disk/by-id/ata-ST5000DM000-1FK178_W4J1VWW3";
+  boot.loader.grub.devices = [
+    "/dev/disk/by-id/ata-ST5000DM000-1FK178_W4J1VWW3"
+    "/dev/disk/by-id/ata-HITACHI_HUA723030ALA640_YVHJJJ3A"
+
+    # warning: this GPT partition label contains no BIOS Boot Partition; embedding won't be possible.
+    # error: embedding is not possible, but this is required for cross-disk install.
+    # SMKR6000S5xeN7.2 S4D082B90000K602DFFX
+    # "/dev/disk/by-id/wwn-0x5000c5008e493b33"
+  ];
 
   # Avoid dmesg errors like
   # ACPI Error: No handler for Region [POWS] (000000008ed47a71) [IPMI]
