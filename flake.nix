6346fbd3d788a786bf2f9467a1a2e5df0b867923
Ivan Jager <aij+git@mrph.org>
2022-08-08 17:57:51 -0500

    Experimenting with Nix flakes
    Test build with
        nix flake check --impure

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    stable.url = "git+file:stable"; #"nixpkgs/nixos-22.05";
+    unstable.url = "git+file:unstable"; #"nixpkgs/nixos-unstable-small";
+  };
+
+  outputs = { self, stable, unstable }: {
+    nixosConfigurations =
+      let
+        mkSystemConfigs = system: m:
+          let
+            name = baseNameOf m;
+            conf = { inherit system; modules = [ m ]; };
+          in
+          {
+            "${name}-stable" = stable.lib.nixosSystem conf;
+            "${name}-unstable" = unstable.lib.nixosSystem conf;
+          };
+
+        x64 = m: mkSystemConfigs "x86_64-linux" m;
+
+        lib = unstable.lib;
+        machines = lib.fold lib.mergeAttrs { } [
+          (x64 machines/tobati)
+          (x64 machines/ita)
+          (x64 ./machines/m0)
+          (x64 ./machines/m1)
+          (x64 ./machines/m2)
+          (x64 ./machines/m4)
+          (x64 ./machines/m5)
+          (x64 ./machines/m6)
+          (x64 ./machines/m7)
+        ];
+
+        # systemArgs = {
+        #   tobati = x64 ./machines/tobati;
+        # };
+        # machines = nixpkgs: {
+        #   tobati = nixpkgs.lib.nixosSystem { system = x64; modules = [ ./machines/tobati ]; };
+        # };
+        # in
+        # machines unstable;
+      in
+      machines // (with machines; {
+        tobati = tobati-unstable;
+        ita = ita-stable;
+        m0 = m0-stable;
+        m1 = m1-stable;
+        m2 = m2-stable;
+      });
+  };
+}
