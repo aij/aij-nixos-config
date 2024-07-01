62b04057e91506c47ea3351e4e6d601ba93895ff
Ivan Jager <aij+git@mrph.org>
2024-07-01 07:56:12 -0500

    Update/upgrade nextcloud to Nextcloud 28

--- nextcloud.nix
+++ nextcloud.nix
@@ -3,7 +3,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud27;
+    package = pkgs.nextcloud28;
     hostName = "nextcloud.mrph.org";
     config = {
       extraTrustedDomains = [
