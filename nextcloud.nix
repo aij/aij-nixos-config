4ca1a3eaf6f113c829b317a34bcfabaffb09a1f6
Ivan Jager <aij+git@mrph.org>
2025-05-30 20:37:41 -0500

    nextcloud: Upgrade to nextcloud30

--- nextcloud.nix
+++ nextcloud.nix
@@ -3,7 +3,7 @@
 
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud29;
+    package = pkgs.nextcloud30;
     hostName = "nextcloud.mrph.org";
     config = {
       extraTrustedDomains = [
