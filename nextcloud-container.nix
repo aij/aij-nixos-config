f8f10f503a9b6db2f57dd1781d933febed969cc7
Ivan Jager <aij+git@mrph.org>
2025-11-18 22:32:24 -0600

    nextcloud-container: Increase startup timeout
    Last upgrade was awkward and required manual intervention
    to disable maintenance mode and re-run the upgrade service
    
    ```
    Nov 18 21:23:56 m2 container nextcloud[1779624]: [ ***  ] A start job is running for nextcloud-setup.service (54s / no limit)
    Nov 18 21:23:56 m2 container nextcloud[1779624]: [  *** ] A start job is running for nextcloud-setup.service (54s / no limit)
    Nov 18 21:23:57 m2 container nextcloud[1779624]: [   ***] A start job is running for nextcloud-setup.service (55s / no limit)
    Nov 18 21:23:57 m2 container nextcloud[1779624]: [    **] A start job is running for nextcloud-setup.service (55s / no limit)
    Nov 18 21:23:58 m2 container nextcloud[1779624]: [     *] A start job is running for nextcloud-setup.service (56s / no limit)
    Nov 18 21:23:58 m2 container nextcloud[1779624]: [    **] A start job is running for nextcloud-setup.service (56s / no limit)
    Nov 18 21:23:59 m2 container nextcloud[1779624]: [   ***] A start job is running for nextcloud-setup.service (57s / no limit)
    Nov 18 21:23:59 m2 container nextcloud[1779624]: [  *** ] A start job is running for nextcloud-setup.service (57s / no limit)
    Nov 18 21:24:00 m2 container nextcloud[1779624]: [ ***  ] A start job is running for nextcloud-setup.service (58s / no limit)
    Nov 18 21:24:00 m2 container nextcloud[1779624]: [***   ] A start job is running for nextcloud-setup.service (58s / no limit)
    Nov 18 21:24:01 m2 container nextcloud[1779624]: [**    ] A start job is running for nextcloud-setup.service (59s / no limit)
    Nov 18 21:24:01 m2 systemd[1]: container@nextcloud.service: start operation timed out. Terminating.
    Nov 18 21:24:01 m2 systemd-nspawn[1779624]: Trying to halt container. Send SIGTERM again to trigger immediate termination.
    ```

--- nextcloud-container.nix
+++ nextcloud-container.nix
@@ -8,6 +8,9 @@
       "/var/lib/postgresql" = { isReadOnly = false; hostPath = "/tank/srv/nextcloud/postgresql/"; };
       "/srv/secrets/" = { isReadOnly = true; hostPath = "/tank/srv/nextcloud/secrets/"; };
     };
+    # Startup can take extra long during upgrades. Default of 1min was
+    # not enough for the upgrade from nextcloud30 -> nextcloud31
+    timeoutStartSec = "30min";
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
