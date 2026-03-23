ed89d8ab41a884ec4c12fcf38d929189501b7175
Ivan Jager <aij+git@mrph.org>
2026-03-22 22:24:25 -0500

    m5: Remove IBM SSD

--- machines/m5/default.nix
+++ machines/m5/default.nix
@@ -15,7 +15,6 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.devices = [
-    "/dev/disk/by-id/ata-MTFDDAK256MAR-1K1AA_90Y8664_90Y8667IBM_0344FDEB-part2"
     "/dev/disk/by-id/wwn-0x5001173100179f60" # Sun F40
     "/dev/disk/by-id/wwn-0x5001173100181ddc" # Sun F40
   ];
