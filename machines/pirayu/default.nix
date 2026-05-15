14570fca1dae6b3b61645fb40cb46d4afc41dbce
Ivan Jager <aij+git@mrph.org>
2026-05-15 16:11:04 -0500

    Revert "pirayu: Enable fingerprint reader (fprintd)"
    This reverts commit 87c0d17d209bcbace9c590391236b3ee3a0bb819.
    
    Technically it works, but PAM requires authentication plugins
    to be tried sequentially, and the integration with xscreensaver
    seems to be especially awkward. (It prompts for a fingerprint
    scan, but provides no feedback on the scan until you hit
    Enter/OK.)

--- machines/pirayu/default.nix
+++ machines/pirayu/default.nix
@@ -187,9 +187,6 @@ networking = {
     };
   });
 
-  # Enable fingerprint reader. Register with fprintd-enroll
-  services.fprintd.enable = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   # environment.systemPackages = with pkgs; [
