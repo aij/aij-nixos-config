a49bd346e0df73b668099d2139db858d5f162bc8
Ivan Jager <aij+git@mrph.org>
2026-07-07 04:40:22 -0500

    update.sh: Handle cmdline args
    Adlso add support for nom-build and clean up cruft.

--- update.sh
+++ update.sh
@@ -1,37 +1,45 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e -x
 
 # TODO: Check repo cleanliness
 
-#(cd unstable && git fetch)
 
-# When using worktrees, this would just re-fetch the same repo, which
-# shouldn't matter since we already have all the updates.
-#(cd stable && git fetch)
-
-function do_update () {
-    cd $1
-    # FIXME: This gets the wrong commit for the stable worktree.
-    old=`git show-ref -s HEAD`
-    git checkout $2
-    # Make sure checkout didn't take us backwards
-    git merge --ff-only $old
-    git merge --ff-only origin/$2
-    cd ..
+function usage() {
+    echo "$0 [stable|unstable] [--nom] [<build_opts>]"
+    exit 1
 }
 
-#do_update unstable nixos-unstable
-#do_update stable nixos-19.09-small
+which=''
+build_opts=()
+nix_build=(time nix-build)
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        stable|unstable)
+            if test -n "$which"; then usage; fi
+            which="$1"
+            shift
+            ;;
+        --nom)
+            nix_build=(nom-build)
+            shift
+            ;;
+        -*)
+            build_opts+=("$1")
+            shift
+            ;;
+    esac
+done
 
 # Is this equivalent, or close enough?
-git submodule update --remote
+git submodule update --remote $which
 
 # Update flake.lock to match
 nix flake update
 
 # Test that everything still builds
-tests/machine-builds.sh
+"${nix_build[@]}" tests/machine-builds.nix "${build_opts[@]}"
 
 # Test colmena can still build my configuration with new submodules
 test -e ../hive.nix && {
@@ -39,4 +47,10 @@ test -e ../hive.nix && {
 }
     
 git add stable unstable flake.lock
-GIT_AUTHOR_NAME=update.sh GIT_COMMITTER_NAME=update.sh git commit -m 'Update nixpkgs'
+
+if test -n "$which"; then
+    msg="Update nixpkgs $which"
+else
+    msg='Update nixpkgs'
+fi
+GIT_AUTHOR_NAME=update.sh GIT_COMMITTER_NAME=update.sh git commit -m "$msg"
